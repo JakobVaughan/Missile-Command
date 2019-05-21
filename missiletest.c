0057020ee75fe5ecce0d1aa8a27b9a27d182c519
MoltenAqua <jakob@ambisonix.org>
2019-05-21 10:38:52 +0100

    4: Comments and Cleaning Up

--- missiletest.c
+++ missiletest.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include <math.h>
 
-void armageddon(int x, int y, int m)
+void armageddon(int x, int y, int m) //Produces an explosion for the cities
 {
 
   mvprintw(x, y, " ");
@@ -20,7 +20,7 @@ void armageddon(int x, int y, int m)
   mvprintw(x - m, (y - 1) + m, "*");
 }
 
-bool interception(int attacky, int attackx, int defencex, int defencey)
+bool interception(int attacky, int attackx, int defencex, int defencey) //checks if the defending missile is close to the attacking
 {
   for (int i = -6; i < 6; i++)
   {
@@ -38,7 +38,7 @@ bool interception(int attacky, int attackx, int defencex, int defencey)
     }
   }
 }
-
+//checks if the missile has reached its target
 bool inRange(int usrx, int usry, int missx, int missy)
 {
   for (int i = -5; i < 5; i++)
@@ -56,11 +56,11 @@ bool inRange(int usrx, int usry, int missx, int missy)
     }
   }
 }
-void fire()
+void fire() //beginnning of missile firing function
 {
 
   int maxrow, maxcol, row, col, x, y, mx, my, c1, c2;
-  mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
+  mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL); //tells program to report mouse position when clicked
   keypad(stdscr, true);
   int c;
   double grad, decx, decy, basex, basey;
@@ -73,6 +73,7 @@ void fire()
   int setx;
   int sety;
   int city;
+  int score = 0;
   int disappear = 0;
   MEVENT event;
   int targets[5];
@@ -81,32 +82,32 @@ void fire()
   targets[2] = 35;
   targets[3] = 47;
   targets[4] = 59;
-  targets[5] = 71;
+  targets[5] = 71; //y coordiantes of the cities
   bool exp = true;
   bool explode = false;
   bool retaliate = false;
   bool isDestroyed = false;
-  getmaxyx(stdscr, maxrow, maxcol);
+  bool boom = false;
+  getmaxyx(stdscr, maxrow, maxcol); //returns maximum x and y coordinate
   int k = 0, m = 0, q = 0, r = 0;
-  while (citiesRemaining>0)
+  while (citiesRemaining>0) //loops provided there are still cities standing
   {
     k = 0;
     m = 0;
     r = 0;
-    city = rand() % 6;
+    city = rand() % 6; //randomises the city chosen
 
     retaliate = false;
-    isDestroyed = false;
+    boom = false;
     //mvprintw(6, 0,"reset k");
 
     //while (k<maxrow-2)
     //{
 
-    while (k < maxrow - 2)
+    while (k < maxrow - 2)//loops until the missile has hit the ground
     {
       
-      //usleep(170000);
-      //refresh();
+      
 
       if (explode)
       {
@@ -129,8 +130,8 @@ void fire()
           explode = false;
         }
       }
-      mvprintw(k - 2, targets[city], " ");
-      mvprintw(k - 1, targets[city], "V");
+      mvprintw(k - 2, targets[city], " ");// clears previous missile position
+      mvprintw(k - 1, targets[city], "V"); //places missile in new position
       c = getch();
       timeout(0);
 
@@ -139,17 +140,17 @@ void fire()
 
         if (getmouse(&event) == OK)
         {
-          if (event.bstate & BUTTON1_CLICKED)
+          if (event.bstate & BUTTON1_CLICKED) //checks if mouse has been clicked
           {
             basex = 41;
-            basey = 17;
+            basey = 17; //starting coordinates for defending missile
             retaliate = true;
           }
         }
       }
       if (retaliate == true)
       {
-        x = event.x;
+        x = event.x; //sets variables to mouse coordinates
         y = event.y;
         //mx = x -41;
         //my = y-17;
@@ -162,39 +163,26 @@ void fire()
         decx = x;
         decy = y;
         disty = 17 - decy;
-        distx = 41 - decx;
-        if ((int)decy <= 17)
+        distx = 41 - decx; //calculates distance to target
+        if ((int)decy <= 17) // provided the mouse position is in a suitable firing range
         {
 
-          //printw("%d, %d, ", event.x, event.y);
-          //printw("test");
-
-          //printw("decx = %f decy = %f", decx, decy);
-          //printw("%d, %f, %d, %f", basex, decx, basey, decy);
-          mvprintw(0, 3, "decy = %f", decy);
+          
 
           mvprintw((int)basey, (int)basex, " ");
 
-          //printw("disty = %d distx = %d", disty, distx);
-          //printw("basex = %d, basey = %d", basex, basey);
-          //basex = basex + (0.2 * decx);
-          //printw("basex = %f basey = %f", basex, basey);
+         
           if (basey > decy)
           {
             basey = basey - (0.2 * (double)disty);
-            basex = basex - (0.2 * (double)distx);
+            basex = basex - (0.2 * (double)distx); // moves missile closer to targets
 
             round(basex);
-            round(basey);
+            round(basey); //rounds number from double to int
             mvprintw((int)basey, (int)basex, "O");
             //printw("basex = %f basey = %f", basex, basey);
             retaliate = true;
-            //usleep(89000);
-            //refresh();
-            //printw("q = %d", q);
-            //printw("disty = %d distx = %d", disty, distx);
-
-            //printw("basex = %d, basey = %d", basex,  basey);
+            
           }
           else
           {
@@ -213,7 +201,7 @@ void fire()
                 mvprintw(basey + r, (basex) + r, "*"); //br
                 mvprintw(basey, (basex) + r, "*");
                 mvprintw(basey - r, (basex) + r, "*"); //bl
-
+                //explosion for the defending missile
                 
                 refresh();
                 usleep(110000);
@@ -228,29 +216,19 @@ void fire()
                 mvprintw(basey-r, (basex)+r, " ");
                 if (r==3)
                 {
-                    if (interception(k-2, targets[city], basex, basey))
+                    if (interception(k-2, targets[city], basex, basey)) //Checks if def missile hits attacking
                     {
                       mvprintw(k-1, targets[city], " ");
-                      isDestroyed = true;
-                      break;
+                      boom = true;
+                      score++;
+                      mvprintw(7,0, "Score = %d", score); //updates score
+                      break; //as missile has been destroyed it cannot hit ground, hence the break
                     }
                 }
 
               
                 r++;
-                /*
-                if (interception(k-2, targets[city], (int)basex, (int)basey) && disappear == 1 && (int)basex != 0 && (int)basey != 0)
-                {
-                  printw("%d = %d, %d = %d", k-2, targets[city], basex, basey);
-                  printw("YEEEEETTTT");
-                  
-                }
-                else
-                {
-                  //printw("%d = %d, %d = %d", k-2, basex, targets[city], basey);
-                  //printw("NOOOO");
-                }
-                */
+                
                 
             }
           }
@@ -267,23 +245,27 @@ void fire()
       usleep(130000);
      
     }
-  if (!isDestroyed)
+  if (!boom)
   {
     for (int p=0; p<3; p++)
     {
-      armageddon(k, targets[city], p);
+      armageddon(k, targets[city], p); //explodes city if a missile has broken through
       
       
     }
     
     citiesRemaining--;
-    mvprintw(8, 0, "cities: %d", citiesRemaining);
+    mvprintw(8, 0, "cities: %d", citiesRemaining); //keeps track of number of cities
   
     //printw("EXITED");
     explode = true;
   }
     //printw("breaking!");
   }
+
+  mvprintw(9, 41, "Your Score was %d", score);
+  refresh();
+  usleep(1700000); //end of game
 }
 
 void mapz(int level)
@@ -293,6 +275,7 @@ void mapz(int level)
   getmaxyx(stdscr, r, c);
 
   r = r - level;
+  //layout for the map
   for (int i = 1; i < c + 1; i++)
   {
 
@@ -300,7 +283,7 @@ void mapz(int level)
     {
       if ((i > 9 && i < 15) || (i > 21 && i < 27) || (i > 33 && i < 39) || (i > 45 && i < 51) || (i > 57 && i < 63) || (i > 69 && i < 75))
       {
-        mvprintw(r - 1, i - 1, "@");
+        mvprintw(r - 1, i - 1, "@"); //2nd row up on map
       }
     }
     else if (level == 3)
@@ -314,7 +297,7 @@ void mapz(int level)
     {
       if ((i > 11 && i < 13) || (i > 23 && i < 25) || (i > 35 && i < 37) || (i > 47 && i < 49) || (i > 59 && i < 61) || (i > 71 && i < 73))
       {
-        mvprintw(r - 1, i - 1, "^");
+        mvprintw(r - 1, i - 1, "^"); 
       }
     }
     else
@@ -325,11 +308,11 @@ void mapz(int level)
         mvprintw(r - 2, i - 1, "^");
         mvprintw(r - 3, i - 1, "^");
         mvprintw(r - 4, i - 1, "^");
-        mvprintw(r - 5, i - 1, "^");
+        mvprintw(r - 5, i - 1, "^"); //sets missile base
       }
       else
       {
-        mvprintw(r - 1, i - 1, "#");
+        mvprintw(r - 1, i - 1, "#"); //sets floor
       }
     }
     /*
@@ -359,88 +342,19 @@ int main()
   noecho();
   cbreak();
   mousemask(ALL_MOUSE_EVENTS, NULL);
-  getmaxyx(stdscr, maxrow, col);
-  printw("col = %d", col);
+  getmaxyx(stdscr, maxrow, col); //ncurses declaration commands
+  //printw("col = %d", col);
 
   for (int j = 0; j < 5; j++)
   {
     mapz(j);
     refresh();
-  }
+  } //creates 5 rows to set map
 
-  while (1)
-  {
+  
 
     fire();
-    //usleep(12000);
-  }
-  //ch = wgetch(stdscr);
-  //if (ch == 113)
-  //{
-  //break;
-  //}
-  /*
-      switch(ch)
-      {
-        case 27:
-        //printw("entered case statement");
-
-        if(getmouse(&event) == OK)
-        {
-          //printw("getmouse(&event) is OK");
-          if (event.bstate & BUTTON1_PRESSED)
-          {
-            mvprintw(maxrow-1, 0, "%s", thing);
-            getyx(stdscr, col, row);
-            //col = 1;
-            //row = 1;
-            
-            sprintf(thing, "row=%d ", row);
-            mvprintw(maxrow-1, 0, "%s", thing);
-            sprintf(thing, " col=%d", col);
-            mvprintw(maxrow-1, 5, "%s", thing);    
-            refresh(); 
-
-          }
-        
-        }
-        //break;
-        
-      }
-      */
-
-  // cbreak(); // allows you to use ctrl + z etc
-  //used if creating test programs
-  /* keypad(stdscr, TRUE);
-    start_color(); //shows you intend to use colour
-    init_pair(1, COLOR_RED, COLOR_BLUE); //middle = foreground, end = background
-    attron(COLOR_PAIR(1)); //sets them in the attributes
-    /*
-    - A_STANDOUT - makes things standout
-    - A_UNDERLINE - makes things underline
-    - COLOR_RED - makes text red
     
-   mvprintw(12, 40, "Type a character");//sets coordinates and prints to the screen
-   attroff(COLOR_PAIR(1)); //turns attribute off
-   mvprintw(row-2, 0, "This screen has %d rows and %d columns\n", row, col);
-   ch = getch();
-   if (ch == KEY_F(1))
-   {
-       printw("F1 key pressed");
-   }
-   else
-  // {
-    //   printw("The pressed key is ");
-      // attron(A_BOLD);
-       //printw("%c", ch);
-       //attroff(A_BOLD);   
-   //.}
-  */
-  //    printw("Hello World! %d\n", thing);
-  //  addch('a');
-  //move(12,13); //in form (y, x)!!!!!!!!
-  //mvprintw(15,20, "Movement");
-  //mvaddch(12, 50, '@');
 
   getch();  //creates a system pause for user interaction, press any key to continue
   endwin(); //frees memory under initscr and closes ncurses library
